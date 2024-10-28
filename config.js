2c9ae91b2450f3605ced73299417d5e47d50b1d9
Siraj617 <mohamedsirajily@gmail.com>
2024-10-28 11:28:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     dbUri: 'mongodb+srv://elearn_2024:gYClToUbfSBAR6w6@elearn.eoptjjn.mongodb.net/AiGpt',
-    port: 9000,
+    port: 5000,
     jwtSecret: 'Siraj@E-learn_2002'
 };
