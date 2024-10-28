ab538c01b1bfbf7fe0958d7a7b7612ee3dc948d6
Siraj617 <mohamedsirajily@gmail.com>
2024-10-28 11:28:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true
     .catch(err => console.log(err));
     
 
-const PORT = config.port || 8000;
+const PORT = config.port || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
