2dda0aca010ea9f9819d143671a4cca228c5ae1c
Siraj617 <mohamedsirajily@gmail.com>
2024-11-03 14:01:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const config = require('./config');
 const Task = require('./model/Getdailytask');
 
