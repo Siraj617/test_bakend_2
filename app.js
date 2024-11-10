51036f5fb3d32f56ea8434743a264b3de3ae5f14
Siraj617 <mohamedsirajily@gmail.com>
2024-11-10 17:08:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,50 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const config = require('./config');
-const Task = require('./model/Getdailytask');
+const Task = require('./model/Getdailytask')
 
 const skillRoutes = require('./routes/skillRoutes');
 
 const app = express();
 
 // Middleware
-const corsOptions = {
-    origin: ['https://e-workspace-peach.vercel.app', 'http://localhost:3000'],
+app.use(cors({
+    origin: 'http://localhost:3000',
     credentials: true,
-};
-app.use(cors(corsOptions));
-app.use(cookieParser());
+}));    
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://e-workspace-peach.vercel.app', 'http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, csrf-token');
-    next();
-});
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the API root endpoint');
-});
-
 // Routes
+
 app.use('/api/skills', skillRoutes);
 app.use('/api/startinterview/', skillRoutes);
-app.use('/api/SaveCode/', skillRoutes);
-app.use('/api/getSavedCode/', skillRoutes);
-app.use('/api/start-interview/', skillRoutes);
-app.use('/api/preBookedUser/', skillRoutes);
-app.use('/api/checkBooking/', skillRoutes);
-app.use('/api/Aiservice/', skillRoutes);
-app.use('/api', skillRoutes);
-app.use('/api/fcm', skillRoutes);
+app.use('/api/SaveCode/', skillRoutes)
+app.use('/api/getSavedCode/', skillRoutes)
+// app.use('/api/start-interview/', skillRoutes)
+app.use('/api/preBookedUser/', skillRoutes)
+app.use('/api/checkBooking/', skillRoutes)
+app.use('/api/Aiservice/', skillRoutes)
+app.use('/api', skillRoutes)
+app.use('/api/fcm', skillRoutes)
+app.use('/api/GetcourseData', skillRoutes)
 
 app.get('/api/getDailyTasks/today', async (req, res) => {
     try {
@@ -57,14 +40,17 @@ app.get('/api/getDailyTasks/today', async (req, res) => {
     }
 });
 
+// app.use('/api/getDailyTasks', skillRoutes); // This mounts the route for daily tasks
+
 
 
 // Connect to MongoDB
 mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
+    
 
-const PORT = config.port || 5000;
+const PORT = config.port || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
